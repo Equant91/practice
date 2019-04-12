8f4c64d92427d881cf41cec800b57dcec0687999
Equant <p.s.ihozkvazy@gmail.ru>
2019-04-12 18:28:12 +0300

    Добавил sequence, и отдельню таблицу с документами.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,17 +1,20 @@
-INSERT INTO doc ( id, doc_code, doc_name)
-VALUES (1, 3, 'Свидетельство о рождение' );
+INSERT INTO handbook_doc ( id, doc_code, doc_name)
+VALUES (handbook_doc_seq.nextval, 3, 'Свидетельство о рождение' );
 
 INSERT INTO country(id, citizenship_code, citizenship_name)
-VALUES ( 1, 643, 'Российская Федерация' );
+VALUES ( country_seq.nextval, 643, 'Российская Федерация' );
 
 INSERT INTO organization (id, version, name, full_name, inn, kpp, address)
-VALUES ( 1,0, 'Гармония', 'Центр социального обслудивания Гармония', 3903243212, 321456123, 'ул.Цюрупы'  );
+VALUES ( org_seq.nextval,0, 'Гармония', 'Центр социального обслудивания Гармония', 3903243212, 321456123, 'ул.Цюрупы'  );
 
 INSERT INTO office(id, version, name, address, phone)
-VALUES ( 1, 0,'Центральный','ул.Цюрупы', 567645  );
+VALUES ( office_seq.nextval, 0,'Центральный','ул.Цюрупы', 567645  );
 
-INSERT INTO user(id, version, office_id, first_name, position, phone, doc_id, doc_number, doc_date, country_id)
-VALUES (1, 0, 1, 'Андрей', 'менеджер', 796412, 1, 123212, '2003-03-30', 1);
+INSERT INTO certain_doc(id, id_handbook_doc, doc_number, doc_date)
+VALUES (certain_doc_seq.nextval,handbook_doc_seq.currval, 123212, '2003-03-30' );
+
+INSERT INTO user(id, version, office_id, first_name, position, phone, certain_doc_id, country_id)
+VALUES (user_seq.nextval, 0, office_seq.currval, 'Андрей', 'менеджер', 796412, certain_doc_seq.currval,  country_seq.currval);
 
 INSERT INTO organization_office(org_id, office_id)
-VALUES( 1,1 );
+VALUES( org_seq.currval,office_seq.currval );
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS organization(
-  id        INTEGER NOT NULL COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
+  id        INTEGER NOT NULL COMMENT 'Уникальный идентификатор' PRIMARY KEY,
   version   INTEGER     NOT NULL COMMENT 'Служебное поле hibernate',
   name      VARCHAR(30) NOT NULL COMMENT 'Название организации',
   full_name VARCHAR(50) NOT NULL COMMENT 'Полное Название организации',
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS organization(
 COMMENT ON TABLE organization IS 'Организация';
 
 CREATE TABLE IF NOT EXISTS office(
-  id        INTEGER NOT NULL COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
+  id        INTEGER NOT NULL COMMENT 'Уникальный идентификатор' PRIMARY KEY,
   version   INTEGER     NOT NULL COMMENT 'Служебное поле hibernate',
   name      VARCHAR(30)  COMMENT 'Название офиса',
   address   VARCHAR(50)  COMMENT 'Адрес офиса',
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS organization_office(
 COMMENT ON TABLE organization_office IS 'join-таблица для связи офиса и организации';
 
 CREATE TABLE IF NOT EXISTS user(
-  id               INTEGER  NOT NULL COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
+  id               INTEGER  NOT NULL COMMENT 'Уникальный идентификатор' PRIMARY KEY,
   version          INTEGER     NOT NULL COMMENT 'Служебное поле hibernate',
   office_id        INTEGER     NOT NULL COMMENT 'Уникальный идентификатор офиса',
   first_name       VARCHAR(30) NOT NULL COMMENT 'Имя',
@@ -37,40 +37,57 @@ CREATE TABLE IF NOT EXISTS user(
   middle_name      VARCHAR(30) COMMENT 'Второе имя',
   position         VARCHAR(30) NOT NULL COMMENT 'Должность',
   phone            INTEGER COMMENT 'Телефон',
-  doc_id           INTEGER COMMENT 'Уникальный идентификатор документа',
-  doc_number       VARCHAR(20) COMMENT 'Номер документа',
-  doc_date         DATE COMMENT 'Дата документа',
+  certain_doc_id   INTEGER COMMENT 'Уникальный идентификатор конкретного документа',
   country_id       INTEGER COMMENT 'Уникальный идентификатор страны гражданства',
   isIdentified     BOOLEAN COMMENT 'Индефицирован'
 );
 COMMENT ON TABLE user IS 'Работник';
 
-CREATE TABLE IF NOT EXISTS doc(
-  id       INTEGER NOT NULL COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
-  doc_code INTEGER NOT NULL COMMENT 'Код документа',
+CREATE TABLE IF NOT EXISTS certain_doc(
+  id              INTEGER NOT NULL COMMENT 'Уникальный идентификатор' PRIMARY KEY,
+  id_handbook_doc INTEGER NOT NULL COMMENT 'Уникальный идентификатор документа из справочника',
+  doc_number      VARCHAR(20) COMMENT 'Номер документа',
+  doc_date        DATE COMMENT 'Дата документа'
+);
+COMMENT ON TABLE certain_doc IS 'Документ';
+
+CREATE TABLE IF NOT EXISTS handbook_doc(
+  id       INTEGER     NOT NULL COMMENT 'Уникальный идентификатор' PRIMARY KEY,
+  doc_code INTEGER     NOT NULL COMMENT 'Код документа',
   doc_name VARCHAR(30) NOT NULL COMMENT 'Название документа'
 );
-COMMENT ON TABLE doc IS 'Справочник документов';
+COMMENT ON TABLE handbook_doc IS 'Справочник документов';
 
 CREATE TABLE IF NOT EXISTS country(
-  id               INTEGER COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT,
+  id               INTEGER COMMENT 'Уникальный идентификатор' PRIMARY KEY,
   citizenship_code INTEGER NOT NULL COMMENT 'Номер страны',
   citizenship_name VARCHAR(30) NOT NULL COMMENT 'Название страны',
 );
-COMMENT ON TABLE doc IS 'Справочник стран';
+COMMENT ON TABLE handbook_doc IS 'Справочник стран';
 
 
-CREATE INDEX IX_Organization_Office_Id ON organization_office(org_id);
+CREATE INDEX IX_Organization_Office_orgId ON organization_office(org_id);
 ALTER TABLE organization_office ADD FOREIGN KEY (org_id) REFERENCES organization(id);
 
-CREATE INDEX IX_Office_Organization_Id ON organization_office(office_id);
+CREATE INDEX IX_Office_Organization_officeId ON organization_office(office_id);
 ALTER TABLE organization_office ADD FOREIGN KEY (office_id) REFERENCES office(id);
 
 CREATE INDEX IX_User_OfficeId ON user (office_id);
 ALTER TABLE user ADD FOREIGN KEY (office_id) REFERENCES office(id);
 
-CREATE INDEX IX_User_DocId ON user (doc_id);
-ALTER TABLE user ADD FOREIGN KEY (doc_id) REFERENCES doc(id);
-
 CREATE INDEX IX_User_CountryId ON user (country_id);
 ALTER TABLE user ADD FOREIGN KEY (country_id) REFERENCES country(id);
+
+CREATE INDEX IX_User_DocId ON user (certain_doc_id);
+ALTER TABLE user ADD FOREIGN KEY (certain_doc_id) REFERENCES certain_doc(id);
+
+CREATE INDEX IX_Certain_doc_Id_handbook_doc ON certain_doc (id_handbook_doc);
+ALTER TABLE certain_doc ADD FOREIGN KEY (id_handbook_doc) REFERENCES handbook_doc(id);
+
+
+CREATE SEQUENCE org_seq;
+CREATE SEQUENCE office_seq;
+CREATE SEQUENCE user_seq;
+CREATE SEQUENCE handbook_doc_seq;
+CREATE SEQUENCE country_seq;
+CREATE SEQUENCE certain_doc_seq;
